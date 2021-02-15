39cb66ee4d5aeae9c55223ed89ff016b4ceee254
yuusasaki <yuu.s.yokohama@gmail.com>
2021-02-15 12:12:03 +0900

    Initial Commit

---
+++ QuizuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizuApp
+//
+//  Created by 佐々木悠 on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
